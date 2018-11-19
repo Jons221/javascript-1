e9c1fe5e1552c481de60f4bcfed333dfd571a75a
John Doe <johndoe@example.com>
2018-11-19 04:52:47 -0500

    javascript-1

--- script.js
+++ script.js
@@ -1,52 +1,80 @@
-console.log('hallo');
-confirm('hallo')
+// console.log('hallo');
+// confirm('hallo')
 
-let firstText = "this is a string";
-let secondText = "another string";
-let combinedText = `${firstText} and ${secondText}`;
-console.log(combinedText);
+// let firstText = "this is a string";
+// let secondText = "another string";
+// let combinedText = `${firstText} and ${secondText}`;
+// console.log(combinedText);
 
-let pureDecimal = 2;
-let floatNumber = 2.24;
+// let pureDecimal = 2;
+// let floatNumber = 2.24;
 
-pureDecimal = 2+2;
-floatNumber = 1+2+4;
-console.log(pureDecimal)
+// pureDecimal = 2+2;
+// floatNumber = 1+2+4;
+// console.log(pureDecimal)
 
-let object ={
-    name: "michael tamsil",
-    universiy: "UIB",
-    year: 2018,
-    friends: ["Mark","Jessy","Shawn"]
-};
-console.log(object);
-console.log(object.universiy);
+// let object ={
+//     name: "michael tamsil",
+//     universiy: "UIB",
+//     year: 2018,
+//     friends: ["Mark","Jessy","Shawn"]
+// };
+// console.log(object);
+// console.log(object.universiy);
 
-moment.locale('id');
-let tgl = moment();
-console.log(moment().format('LLLL'));
-let tgl2 = tgl.format('dddd, YYYY DD MMMM');
-let pesan = `hari ini adalah ${tgl2}`
-console.log(pesan);
+// moment.locale('id');
+// let tgl = moment();
+// console.log(moment().format('LLLL'));
+// let tgl2 = tgl.format('dddd, YYYY DD MMMM');
+// let pesan = `hari ini adalah ${tgl2}`
+// console.log(pesan);
 
-let grade = window.prompt("MY grade is")
-let result = '';
-if (grade >= 90) {
-    result ="A";
+// let grade = window.prompt("MY grade is")
+// let result = '';
+// if (grade >= 90) {
+//     result ="A";
+// }
+// else if (grade >= 80){
+//     result ="B";
+// }
+// else if (grade >= 70)
+// {
+//     result ="c";
+// }
+// else if (grade >= 60)
+// {
+//     result ="D";
+// }
+// else
+// {
+//     result ="F";
+// }
+// window.alert (`your score is ${result}`)
+
+const sentenceA = "Hello World!";
+const sentenceB = " Good Bye!";
+
+console.log(sentenceA) ;
+console.log(sentenceB) ;
+
+const sentenceC = sentenceA + " " + sentenceB;
+const sentenceD = `${sentenceA} ${sentenceB}`;
+
+
+console.log(sentenceA.toUpperCase(sentenceA));
+console.log(sentenceB.toLowerCase(sentenceB));
+
+Array = ["BMW","VOLVO","Saab","Ford"];
+for (i=0; i < Array.length; i++){
+    let mobil = Array[i];
+    console.log(mobil);
 }
-else if (grade >= 80){
-    result ="B";
-}
-else if (grade >= 70)
-{
-    result ="c";
-}
-else if (grade >= 60)
-{
-    result ="D";
-}
-else
-{
-    result ="F";
-}
-window.alert (`your score is ${result}`)
+const names =[
+    "Mr. Fahri",
+    "Mr. Abdul",
+    "Mrs.Josephhine",
+    "Mr. Joseph",
+    "Mr Paul",
+    "Mrs. Paula",
+    "Mr. Hakim"
+]
